83f6134ffc140ef510de0c96ec4094e617150c5d
Osvaldo Lucunde <osvaldoluc7@gmail.com>
2026-09-25 23:51:18 +0100

    Placing pieces on the board

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -14,12 +14,13 @@ public class UI {
         }
         System.out.println("  a b c d e f g h");
     }
+
     public static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("_");
         }
         else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
         System.out.print(" ");
     }
